f700ba0ba22eeab40c7e685b080a074690f32585
Alex Schearer <aschearer@gmail.com>
2014-01-25 11:47:08 -0800

    Adding hit detection for tiles in board

---
+++ Unity/Assets/Boards/TileSelection.cs
@@ -0,0 +1,28 @@
+﻿namespace Assets.Boards
+{
+    using UnityEngine;
+
+    public class TileSelection : MonoBehaviour
+    {
+        private GameObject selected;
+
+        public void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit))
+                {
+                    this.selected = hit.collider.gameObject;
+                    this.selected.tag = "Selected";
+                }
+            }
+            else if (Input.GetMouseButtonUp(0) && this.selected != null)
+            {
+                this.selected.tag = null;
+                this.selected = null;
+            }
+        }
+    }
+}
