4acb8bf06e2c091c5fe9b14d160b491d0c34b01c
Alex Schearer <aschearer@gmail.com>
2014-01-25 11:19:58 -0800

    Adding board and tile prefabs, script to create tiles at start

---
+++ Unity/Assets/Boards/BoardBuilder.cs
@@ -0,0 +1,30 @@
+﻿namespace Assets
+{
+    using UnityEngine;
+
+    public class BoardBuilder : MonoBehaviour
+    {
+        public int NumberOfColumns = 4;
+
+        public int NumberOfRows = 4;
+
+        public GameObject TilePrefab;
+
+        public void Start()
+        {
+            for (int col = 0; col < this.NumberOfColumns; col++)
+            {
+                for (int row = 0; row < this.NumberOfRows; row++)
+                {
+                    var position = new Vector3(col, 0, row);
+                    var tile = (GameObject)GameObject.Instantiate(this.TilePrefab, position, Quaternion.identity);
+                    tile.transform.parent = this.transform;
+                }
+            }
+        }
+
+        public void Update()
+        {
+        }
+    }
+}
