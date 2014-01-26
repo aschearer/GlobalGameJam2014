e0f795abb673189574deec112c24b0511e8cfca3
Alex Schearer <aschearer@gmail.com>
2014-01-26 08:57:19 -0800

    Units collide and destroy one another

---
+++ Unity/Assets/Units/CombatUnit.cs
@@ -0,0 +1,12 @@
+﻿namespace Assets.Units
+{
+    using UnityEngine;
+
+    public class CombatUnit : MonoBehaviour
+    {
+        public void OnCollisionEnter(Collision collision)
+        {
+            GameObject.Destroy(collision.collider.gameObject);
+        }
+    }
+}
