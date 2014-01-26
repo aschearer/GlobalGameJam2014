853d06c006025c36705cd15a592e85177498dcae
Pat Kemp <pk@patkemp.com>
2014-01-26 12:54:30 -0800

    Adding stables

---
+++ Unity/Assets/SpeedUpUnit.cs
@@ -0,0 +1,24 @@
+﻿namespace Assets.Units
+{
+	using Assets.Store;
+	
+	using UnityEngine;
+	
+	public class SpeedUpUnit : MonoBehaviour
+	{
+		private int unitLayer;
+		
+		public void Start()
+		{
+			this.unitLayer = LayerMask.NameToLayer("Units");
+		}
+		
+		public void OnTriggerEnter(Collider other)
+		{
+			if (other.gameObject.layer == this.unitLayer)
+			{
+				other.gameObject.SendMessage("SpeedUp");
+			}
+		}
+	}
+}
