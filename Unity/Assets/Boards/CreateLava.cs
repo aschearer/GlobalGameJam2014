22f748eecd11f1a7bebaf5e0a0c064e61bacfdf2
Alex Schearer <aschearer@gmail.com>
2014-01-26 11:45:01 -0800

    Spawning lava randomly

---
+++ Unity/Assets/Boards/CreateLava.cs
@@ -0,0 +1,44 @@
+﻿namespace Assets.Boards
+{
+    using Assets.Tiles;
+
+    using UnityEngine;
+
+    public class CreateLava : MonoBehaviour
+    {
+        public float TimeTillNextLava = 8;
+
+        public GameObject LavaPrefab;
+
+        private float elapsedTime;
+
+        public void Update()
+        {
+            this.elapsedTime += Time.deltaTime;
+            while (this.elapsedTime > this.TimeTillNextLava)
+            {
+                this.elapsedTime -= this.TimeTillNextLava;
+                this.AddLava();
+            }
+        }
+
+        private void AddLava()
+        {
+            var emptyTiles = GameObject.FindGameObjectsWithTag(SelectedTile.EmptyTagName);
+            int r = Random.Range(0, emptyTiles.Length);
+            var tile = emptyTiles[r];
+
+            Debug.Log("Adding lava");
+            var position = tile.transform.position;
+            position.y += 0.5f;
+            var goldMine = (GameObject)GameObject.Instantiate(
+                this.LavaPrefab,
+                position,
+                this.LavaPrefab.transform.rotation);
+            goldMine.transform.parent = tile.transform;
+            goldMine.transform.localPosition = Vector3.zero;
+
+            tile.tag = SelectedTile.OccupiedTagName;
+        }
+    }
+}
---
+++ Unity/Assets/Buildings/Timeout.cs
@@ -0,0 +1,33 @@
+﻿namespace Assets.Buildings
+{
+    using Assets.Tiles;
+
+    using UnityEngine;
+
+    public class Timeout : MonoBehaviour
+    {
+        public float TimeToLive = 10;
+
+        private float elapsedTime;
+
+        private int tileLayer;
+
+        public void Start()
+        {
+            this.tileLayer = LayerMask.NameToLayer("Tiles");
+        }
+
+        public void Update()
+        {
+            this.elapsedTime += Time.deltaTime;
+            if (this.elapsedTime >= this.TimeToLive)
+            {
+                GameObject.Destroy(this.gameObject);
+                if (this.gameObject.transform.parent.gameObject.layer == this.tileLayer)
+                {
+                    this.gameObject.transform.parent.gameObject.tag = SelectedTile.EmptyTagName;
+                }
+            }
+        }
+    }
+}
