85ea3fff09b6fb8e5fa6544b8849e2ce0f2b40c2
Alex Schearer <aschearer@gmail.com>
2014-01-26 14:24:51 -0800

    Accelerating rotation time when movement speed is increased

--- Unity/Assets/Units/MoveUnit.cs
+++ Unity/Assets/Units/MoveUnit.cs
@@ -9,41 +9,41 @@
 
     public class MoveUnit : MonoBehaviour
     {
-		public float StartSpeed = 10;
+        public float StartSpeed = 10;
 
-		public float MaxSpeed = 30;
+        public float MaxSpeed = 30;
 
-		public float SpeedIncrement = 10;
+        public float SpeedIncrement = 10;
 
-		public float DisplayOffset = 1f;
+        public float DisplayOffset = 1f;
 
-		private float speed;
-		
-		private Vector3 velocity;
+        private float speed;
+        
+        private Vector3 velocity;
 
-		private GameObject model;
+        private GameObject model;
 
         private float targetAngle;
 
-		private float startTime;
+        private float startTime;
 
-		public void Start()
-		{
-		    this.targetAngle = this.transform.rotation.eulerAngles.y;
-			model = this.transform.Find("Model").gameObject;
-			model.transform.localPosition = new Vector3(Random.Range(-DisplayOffset, DisplayOffset),
-			                                            0.5f, 
-			                                            Random.Range(-DisplayOffset, DisplayOffset));
-			startTime = Time.time;
-			speed = StartSpeed;
-			velocity = new Vector3(0, 0, speed);
-		}
+        public void Start()
+        {
+            this.targetAngle = this.transform.rotation.eulerAngles.y;
+            model = this.transform.Find("Model").gameObject;
+            model.transform.localPosition = new Vector3(Random.Range(-DisplayOffset, DisplayOffset),
+                                                        0.5f, 
+                                                        Random.Range(-DisplayOffset, DisplayOffset));
+            startTime = Time.time;
+            speed = StartSpeed;
+            velocity = new Vector3(0, 0, speed);
+        }
 
-		public void SpeedUp()
-		{
-			speed += SpeedIncrement;
-			speed = Mathf.Min(MaxSpeed, speed);
-		}
+        public void SpeedUp()
+        {
+            speed += SpeedIncrement;
+            speed = Mathf.Min(MaxSpeed, speed);
+        }
 
         public void Rotate(float targetAngle)
         {
@@ -59,13 +59,13 @@
         }
 
         public void Update()
-		{
-			this.velocity.z = speed;
+        {
+            this.velocity.z = speed;
 
             var currentAngle = this.transform.rotation.eulerAngles.y;
             if (Mathf.Abs(this.targetAngle - currentAngle) > 0.005f)
             {
-                currentAngle = Mathf.LerpAngle(currentAngle, this.targetAngle, Time.deltaTime * 2);
+                currentAngle = Mathf.LerpAngle(currentAngle, this.targetAngle, Time.deltaTime * 2 * (this.speed / this.StartSpeed));
                 this.transform.rotation = Quaternion.Euler(0, currentAngle, 0);
             }
 
@@ -73,9 +73,9 @@
             position += this.transform.rotation * this.velocity * Time.deltaTime;
             this.rigidbody.MovePosition(position);
 
-			model.transform.localPosition = new Vector3(model.transform.localPosition.x,
-			                                            Mathf.Sin((Time.time - startTime) * 10) * 0.5f + 0.5f, 
-			                                              model.transform.localPosition.z);
+            model.transform.localPosition = new Vector3(model.transform.localPosition.x,
+                                                        Mathf.Sin((Time.time - startTime) * 10) * 0.5f + 0.5f, 
+                                                          model.transform.localPosition.z);
         }
     }
 }
