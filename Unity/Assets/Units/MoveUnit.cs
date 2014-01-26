5d71f28bed06b1a5ea314154c05d060dd2c8ca04
Pat Kemp <pk@patkemp.com>
2014-01-25 18:04:49 -0800

    Added a sin wave bob to units.

--- Unity/Assets/Units/MoveUnit.cs
+++ Unity/Assets/Units/MoveUnit.cs
@@ -14,12 +14,15 @@
 
         private float targetAngle;
 
+		private float startTime;
+
 		public void Start()
 		{	
 			model = this.transform.Find("Capsule").gameObject;
 			model.transform.localPosition = new Vector3(Random.Range(-DisplayOffset, DisplayOffset),
 			                                            0.5f, 
 			                                            Random.Range(-DisplayOffset, DisplayOffset));
+			startTime = Time.time;
 		}
 
         public void Rotate(float targetAngle)
@@ -39,6 +42,10 @@
             var position = this.transform.position;
             position += this.transform.rotation * this.Velocity * Time.deltaTime;
             this.rigidbody.MovePosition(position);
+
+			model.transform.localPosition = new Vector3(model.transform.localPosition.x,
+			                                            Mathf.Sin((Time.time - startTime) * 10) * 0.5f + 0.5f, 
+			                                              model.transform.localPosition.z);
         }
     }
 }
